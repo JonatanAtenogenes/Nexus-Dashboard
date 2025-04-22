c9df46ca7ee4b5bcf402b9e228d42ae87b14f053
Jonatan Atenogenes <jonatan.atenogenes.dev@outlook.com>
2025-04-22 13:37:33 -0600

    Principal configuration for the project

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ9UjivhgYgRsLkQxZ-bCDStdtnFSNv80",
+  authDomain: "nexus-b7be0.firebaseapp.com",
+  projectId: "nexus-b7be0",
+  storageBucket: "nexus-b7be0.firebasestorage.app",
+  messagingSenderId: "622248450549",
+  appId: "1:622248450549:web:bca7020d3c12a8899c28bc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
